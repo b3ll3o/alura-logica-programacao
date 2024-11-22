b2c4f9e78774f47348ef7d84856fc34387cbb7ec
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2024-11-22 11:20:00 -0300

    primeira aula

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('Boas vindas ao jogo do número secreto.');
+
+let numeroSecreto = 9;
+
+let chute = prompt('Escolha um número entre 1 e 10.');
+
+if(numeroSecreto == chute){
+    console.log('Acertou o número secreto.')
+}
