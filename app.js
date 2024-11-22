3b2b1d405b960d3517b1f7cc891791007f44894f
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2024-11-22 11:33:09 -0300

    aula 2

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ let numeroSecreto = 9;
 let chute = prompt('Escolha um número entre 1 e 10.');
 
 if(numeroSecreto == chute){
-    console.log('Acertou o número secreto.')
+    alert('Acertou.')
+}else {
+    alert('Errou.')
 }
