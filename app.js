5b8c650c23a2779f044612a33aed082d1690596c
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2024-11-22 15:57:36 -0300

    joguinho de numero secreto finalizado

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 alert('Boas vindas ao jogo do número secreto.');
 
-let numeroSecreto = 9;
+let rangeNumeros = prompt(`Range de números que deseja jogar:`);
 
-let chute = prompt('Escolha um número entre 1 e 10.');
+let numeroSecreto = parseInt(Math.random() * rangeNumeros + 1);
 
-if(numeroSecreto == chute){
-    alert('Acertou.')
-}else {
-    alert('Errou.')
+let chute = 0;
+
+let tentativas = 1;
+
+console.log(`O número secreto é ${numeroSecreto}.`)
+
+while(true){
+
+    chute = prompt(`Escolha um número entre 1 e ${rangeNumeros}.`);
+
+    if(numeroSecreto == chute){
+        let palavraSingularPlural = tentativas > 1 ? 'tentativas' : 'tentativa';
+        alert(`Acertou depois de ${tentativas} ${palavraSingularPlural}.`);
+        break;
+    }else {
+        if(chute > numeroSecreto){
+            alert('O número secreto é menor.');
+        }else {
+            alert('O número secreto é maior.');
+        }
+        tentativas++;
+    }
 }
